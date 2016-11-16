9f2f87ab299a78ee08867bb0cce54dd0082a62f8
WilliamChung <William@Williams-Mac-mini.local>
2016-11-15 23:02:25 -0800

    setup layout
    setup buttons manually. Should have used stack view:
    https://www.youtube.com/watch?annotation_id=annotation_2968083553&featur
    e=iv&index=8&list=PLMRqhzcHGw1ZkH8RuznGMS0NZs0jWQQ5a&src_vid=5So69J1xeCI
    &v=2KyzuLC-w-8

--- iProfileApp/ViewController.swift
+++ iProfileApp/ViewController.swift
@@ -10,9 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var qButton: UIButton!
+    @IBOutlet weak var wButton: UIButton!
+    @IBOutlet weak var eButton: UIButton!
+    @IBOutlet weak var rButton: UIButton!
+    @IBOutlet weak var tButton: UIButton!
+    @IBOutlet weak var yButton: UIButton!
+    @IBOutlet weak var uButton: UIButton!
+    @IBOutlet weak var iButton: UIButton!
+    @IBOutlet weak var oButton: UIButton!
+    @IBOutlet weak var pButton: UIButton!
+    
+    var tStamp: String {
+        return "\(NSDate().timeIntervalSince1970 * 1000)"
+    }
+    
+    var oldDownTime: Double = 0
+    var currentDownTime: Double = 0
+    var upTime: Double = 0
+    var downTimeDifference: Double = 0
+    var upTimeDifference: Double = 0
+    var shiftClicked: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        qButton.layer.cornerRadius = 5
+        wButton.layer.cornerRadius = 5
+        eButton.layer.cornerRadius = 5
+        rButton.layer.cornerRadius = 5
+        tButton.layer.cornerRadius = 5
+        yButton.layer.cornerRadius = 5
+        uButton.layer.cornerRadius = 5
+        iButton.layer.cornerRadius = 5
+        oButton.layer.cornerRadius = 5
+        pButton.layer.cornerRadius = 5
+        
+    
+    }
+    
+    @IBAction func push(_ sender: UIButton) {
+        
+        oldDownTime = currentDownTime
+        currentDownTime = Double (tStamp)!
+        
+        downTimeDifference = currentDownTime - oldDownTime
+        
+        print(oldDownTime)
+        print(currentDownTime)
+        print(downTimeDifference)
+        
+        //downDownTime.text = String (downTimeDifference)
+        //timeStamp.text = String (tStamp)
+        
+    }
+    
+    
+    @IBAction func release(_ sender: UIButton) {
+        
+        upTime = Double (tStamp)!
+        upTimeDifference = upTime - currentDownTime
+        
+        print(upTime)
+        print(upTimeDifference)
+        
+        
+        //downUpTime.text = String (upTimeDifference)
     }
 
     override func didReceiveMemoryWarning() {
